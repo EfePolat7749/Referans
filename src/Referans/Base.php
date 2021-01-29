e5a0bf5af1bfbd65f11b94b46bca930151612b8d
EfePolat7749 <64210911+EfePolat7749@users.noreply.github.com>
2021-01-29 09:15:10 -0600

    Update Base.php

--- src/Referans/Base.php
+++ src/Referans/Base.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace Referans;
+
+use pocketmine\{
+  plugin\PluginBase,
+  command\CommandSender,
+  command\Command,
+  event\Listener,
+  utils\Config
+};
+use Referans\{
+  Commands\Referans,
+  Event\EventListener
+};
+class Base extends PluginBase implements Listener{
+  
+     private static $instance;
+
+    public function onLoad(){
+        self::$instance = $this;
+    }
+
+    public static function getInstance(): Base{
+        return self::$instance;
+    }
+
+   public function onEnable(){
+  @mkdir($this->getDataFolder()."Referans/");
+ @mkdir($this->getDataFolder()."ReferansOyuncular/");
+ $this->getServer()->getCommandMap()->register("referans", new Referans($this));
+ $this->getServer()->getPluginManager()->registerEvents(new EventListener(),$this);
+   }
+            
+}
